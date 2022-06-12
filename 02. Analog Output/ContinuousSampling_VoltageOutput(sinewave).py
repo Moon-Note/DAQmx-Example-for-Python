fa1deee6aa80d2e78db000f68dc843258bcf9e7c
MoonNote <nationalv@kakao.com>
2022-06-12 22:24:04 +0900

    Create ContinuousSampling_VoltageOutput(sinewave).py

---
+++ 02. Analog Output/ContinuousSampling_VoltageOutput(sinewave).py
@@ -0,0 +1,34 @@
+'''
+Copyleft © MoonNote
+
+작성자 : MoonNote
+블로그 주소 : MoonNote.tistory.com
+'''
+import nidaqmx
+import numpy as np
+import math
+
+from ast import Break
+from nidaqmx.constants import AcquisitionType, Edge
+
+sampling_rate = 1000
+
+with nidaqmx.Task() as task : 
+
+    task.ao_channels.add_ao_voltage_chan("Dev1/ao0", "", -10, 10)
+    task.timing.cfg_samp_clk_timing(sampling_rate, "", active_edge=Edge.RISING, sample_mode=AcquisitionType.CONTINUOUS, samps_per_chan=1000)
+    
+    time = np.arange(0, 2*math.pi, 5*2*math.pi/sampling_rate)
+    amplitude = 5*np.sin(time)
+
+    task.write(amplitude, auto_start=False, timeout=10)
+    
+    task.start()
+    
+    try:
+        while True:
+            task.is_task_done()
+    except KeyboardInterrupt:
+        Break
+        print('close the program')
+    task.stop()
